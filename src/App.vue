e32f5778b832cd0e3b9ac48c40a3c7d689761e13
S0ngbirb <144578453+S0ngbirb@users.noreply.github.com>
2023-10-24 14:59:23 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -89,9 +89,9 @@ export default {
 
   data() {
     return {
-      "mission_slug": "001",
+      "mission_slug": "002",
       "current_md": "",
-      "events": "001",
+      "events": "",
       "missions": [
         {
           "slug": "001",
